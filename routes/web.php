42369506c60aedfeb81ebd90da20461c3d5c0e3c
Dirham Triyadi <dirhamtriyadi@gmail.com>
2022-09-07 13:05:11 +0700

    add adminlte

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index']);
