45a5ea7a4959d6c898bc52517c4f1f085a5b808f
dirhamtriyadi <dirhamtriyadi@gmail.com>
2022-12-03 20:52:16 +0700

    update factories UsersFactory

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -16,6 +16,7 @@ class Users extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'role',
         'name',
         'email',
         'password',
--- database/factories/UsersFactory.php
+++ database/factories/UsersFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
+class UsersFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -19,7 +19,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            // 'role' => fake()->,
+            'role' => fake()->randomElement(['admin', 'surveyor']),
             'email' => fake()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
